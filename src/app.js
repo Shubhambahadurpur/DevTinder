140d603c15be345da26fd54cc72b6e17ad0b840a
Shubham Kumar <shubhambahadurpur48@gmail.com>
2025-07-13 17:47:06 +0530

    enhance user model and validation: add password strength validation, email uniqueness, and user data validation logic

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 require('dotenv').config();
+const { validateSignUpData } = require('./utils/validation')
+const bcrypt = require('bcrypt');
 
 
 app.use(express.json());
@@ -18,17 +20,32 @@ app.get("/user", async (req, res) => {
     const data = await User.findOne({ emailId: emailId });
     res.send(data);
   } catch (err) {
-    res.status(500).send("Internam Server Error");
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.post("/user", async (req, res) => {
-  const user = new User(req?.body);
+app.post("/user", async (req, res) => { // create new user
   try {
+    validateSignUpData(req);
+    const { password } = req.body;
+
+    const encryptPassword = await bcrypt.hash(password, 10);
+    const data = {
+      firstName: req?.body?.firstName,
+      lastName: req?.body?.lastName,
+      emailId: req?.body?.emailId,
+      password: encryptPassword,
+      age: req?.body?.age,
+      gender: req?.body?.gender,
+      profilePhoto: req?.body?.profilePhoto,
+      about: req?.body?.about,
+      skills: req?.body?.skills
+    }
+    const user = new User(data);
     await user.save();
-    res.send("post req");
+    res.send("User created successfully");
   } catch (err) {
-    res.status(500).send("Internal Server Error");
+    res.status(400).send("Internal Server Error "+ err);
   }
 });
 
@@ -42,13 +59,17 @@ app.delete("/user", async (req, res) => {
     }
 });
 
-app.patch("/user", async (req, res) => {
-    const { userId, firstName } = req?.body;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
+    const body = req?.body;
     try {
-        await User.findByIdAndUpdate(userId, { firstName });
+        const ALLOWED_FEILDS =  ['about', 'gender', 'profilePhoto', 'skills'];
+        const IS_ALLOWED = Object.keys(body)?.every(key => ALLOWED_FEILDS.includes(key));
+        if (!IS_ALLOWED) throw new Error("Update not allowed");
+        await User.findByIdAndUpdate(userId, req.body, { runValidators: true });
         res.send("User Updated Successfully");
     } catch (err) {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send("Internal Server Error " + err);
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,67 @@
-const { mongoose } = require('mongoose');
+const { mongoose } = require("mongoose");
+const validator = require("validator")
 
 const userSchema = mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastname: { type: String },
-    emailId: { type: String },
-    password: { type: String },
-    age: { type: Number },
-    gender: { type: String },
-})
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: [3, "Too short name"],
+    maxLength: [50, "Too long name"]
+  },
+  lastName: { 
+    type: String 
+  },
+  emailId: { 
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email")
+      }
+    }
+  },
+  password: { 
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Weak Password");
+      }
+    }
+  },
+  age: { 
+    type: Number,
+    min: [18, "Age should be above 18"],
+    required: true
+  },
+  gender: { 
+    type: String,
+    validate(value) {
+        if(!["male", "female"].includes(value)) {
+            throw new Error("Gender is not valid")
+        }
+    }
+  },
+  profilePhoto: {
+    type: String,
+    default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2XpA_J7m2G2hSns2UcaNwHO1vTC96DqbiGw&s",
+    validate(value) {
+      if (!validator.isURL(value)) {
+        throw new Error("Invalid profile photo url");
+      }
+    }
+  },
+  about: {
+    type: String,
+    default: `Hey I am a developer and I love dev Tinder`,
+    maxLength: 100
+  },
+  skills: {
+    type: [String]
+  }
+}, { timestamps : true });
 
 module.exports = mongoose.model("User", userSchema);
